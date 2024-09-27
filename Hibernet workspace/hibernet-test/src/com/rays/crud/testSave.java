7d2ddc5c4b9ae4a2dff964eeff44f784dc41e47d
Your Name <meghapatidar9066@gmail.com>
2024-09-28 00:47:29 +0530

    code

---
+++ Hibernet workspace/hibernet-test/src/com/rays/crud/testSave.java
@@ -0,0 +1,38 @@
+package com.rays.crud;
+
+import java.util.Date;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import com.rays.user.UserDTO;
+
+public class testSave {
+	public static void main(String[] args) {
+
+		UserDTO dto = new UserDTO();
+		// dto.setId(2);
+		dto.setFirstName("megha");
+		dto.setLastName("xty");
+		dto.setLoginId("xyz@gmail.com");
+		dto.setPassword("456");
+		dto.setDob(new Date());
+		dto.setAddress("bhopal");
+
+		SessionFactory sf = new Configuration().configure().buildSessionFactory();
+
+		Session session = sf.openSession();
+
+		Transaction tx = session.beginTransaction();
+
+		session.save(dto);
+
+		tx.commit();
+
+		session.close();
+
+	}
+
+}
